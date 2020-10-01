3947103bfc18dddc25cc7d74c08005b14731228b
Siva Jyothi Thippireddy <tsiva@corp.ojas-it.com>
2020-10-01 18:50:02 +0530

    recepieappsecond

--- addrecepie.js
+++ addrecepie.js
@@ -1,30 +1,27 @@
-
 let recepieLists = []
 const recepie = {
     name: '',
     description: '',
     ingridients: []
 }
-// let newrecepie=[]
 let recipesAll = "";
-
 // add ingriendient
 function addingridient() {
     let ingriform = document.getElementById('ingredientform')
     if (ingriform.value == 0) {
-        recepie.ingridients = "No Ingridient";
+      recepie.ingridients = "No Ingridient";
     }
     else {
         recepie.ingridients.push(ingriform.value);
     }
-
-    console.log("ghddgfdgfdgsgd", recepie.ingridients)
+    console.log("ghddgfdgfdgsgd")
     let formdivision = document.getElementById('ingredientformdiv');
     let ingri = document.createElement('p');
     let ingredientData = document.createTextNode(recepie.ingridients);
     ingri.appendChild(ingredientData);
     formdivision.appendChild(ingri);
     recepieLists = [];
+
 }
 // add Recepie
 function addnewRecepie() {
@@ -79,29 +76,68 @@ function addnewRecepie() {
         let button1 = document.createElement('button')
         let button2 = document.createElement('button')
         let buttonvalue = document.createTextNode('edit')
-       
         let buttonvalue1 = document.createTextNode('delete')
         button1.appendChild(buttonvalue);
         button2.appendChild(buttonvalue1);
+
         division.appendChild(button1);
         button1.addEventListener("click", function(){
             document.getElementById("recepiename").value= i.name ;
             document.getElementById("recepiesteps").value= i.description;
             document.getElementById("ingredientformdiv").value= i.ingridients;
+            console.log(i.name,"edit")
           });
-
         division.appendChild(button2);
          button2.addEventListener("click", function(){
-          delete i.name;
-          console.log()
+           delete i.name ;
+           delete i.description;
+           delete i.ingridients;
+            console.log(i.name,"dele")
           });
-
     }
     recepieLists = [];
- 
 }
-// document.getElementById("resetData").reset();
+// Search
+function searchfunction(){
+    input = document.getElementById("searchbar").value;
+    // filter = input.value.toUpperCase();
+    const recipesJSON = localStorage.getItem('Recipe')
+    if (recipesJSON === null) {
+        recipesAll = [];
+    } else {
+        const recipesJSON = localStorage.getItem('Recipe')
+        recipesAll = JSON.parse(recipesJSON)
+    }
+    console.log(recipesAll, "search");
+    for (let i of recepieLists) {
+      let filteredList = recipesAll.filter(value => {
+            return value.name == input 
+          });
+          for(let i of filteredList){
+              const filter1=i.name;
+              console.log(filteredList,"aaaaaaaaaaaa");
+          let division1 = document.getElementById('search');
+          let searchname = document.createElement('p');
+          let searchData = document.createTextNode(filter1);
+          searchname.appendChild(searchData);
+          division1.appendChild(searchname);
+          }
+        }
+}
+
+function myFunction() {
+    var x = document.getElementById("addReceipeshow");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+// function removelist(){
+
+// }
+    
 
 
-
-
+       
